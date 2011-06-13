a86275d8caafb8534899a65f9ea540620f3cd310
Carl Boettiger <cboettig@gmail.com>
2011-06-12 20:19:21 -0700

    moved auth_ex file to demo

--- demo/examples.R
+++ demo/examples.R
@@ -1,7 +1,7 @@
 require(RMendeley)
 
 # You must enter your api key. To avoid passing it to each function use:
-options(MendeleyKey="UserAPIkey")
+#options(MendeleyKey=UserAPIkey)
 
 authors(discipline="6") # Discipline "6", Computer Science
 details("10.1088/0953-8984/22/19/194101", type="doi")  
