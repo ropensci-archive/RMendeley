6b9885ff3143a1af2397f9b7ad4c983632277ff2
Carl Boettiger <cboettig@gmail.com>
2012-02-08 12:40:46 -0800

    add support for quotes for exact matches in msearch

--- R/search.R
+++ R/search.R
@@ -14,6 +14,9 @@
 #'  a = msearch("bob", , 20)
 #'  b = msearch("bob", 4, 20)
 #'  d = msearch("authors:Alistair Boettiger published_in:Science")
+#'  # Get the DOIs of Journal of Ecology hits (returns first 999 exact matches)
+#'   all <- msearch("published_in:\"Journal of Ecology\"")
+#'   dois <- sapply(all, function(x) x$doi)
 #' }
 #' @export
 msearch =
@@ -21,8 +24,10 @@ function(query, page = NA, numItems = 1000L, key = getOption("MendeleyKey",
          stop("need an API key for Mendeley")), curl=getCurlHandle(), ...)
 {
 
+   # httr could probably handle this better!
    query <- gsub(":", "%3A", query) # use html symbol for colons
    query <- gsub(" ", "%20", query) # use html symbol for spaces
+   query <- gsub("\"", "%22", query) # use html symbol for quotes
    url = sprintf("%s/%s", "http://api.mendeley.com/oapi/documents/search", query)
    args = list(consumer_key = key)
    if(!is.na(page))
@@ -30,5 +35,6 @@ function(query, page = NA, numItems = 1000L, key = getOption("MendeleyKey",
    if(!is.na(numItems))
       args$items = as.integer(numItems)
    tt = getForm(url, .params = args, .opts=list(...), curl=curl)
-   fromJSON(I(tt))
+   out <- fromJSON(I(tt))
+   out[[1]]
 }
