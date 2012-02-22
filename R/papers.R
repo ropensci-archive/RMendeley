95da6052435d406e8ad22a0138528e6f8d723f4b
Karthik Ram <karthik.ram@gmail.com>
2012-02-22 15:15:01 -0800

    minor formatting

--- R/papers.R
+++ R/papers.R
@@ -3,7 +3,7 @@
 #' @param upandcoming logical: give recent/trending top authors only?
 #' @param url Mendeley API URL (don't change this)
 #' @param key Mendeley API key (otherwise will try and load from package)
-#' @param curl If using in a loop, call getCurlHandle() first and pass 
+#' @param curl If using in a loop, call getCurlHandle() first and pass
 #'  the returned value in here (avoids unnecessary footprint)
 #' @param ... optional additional curl options (debugging tools mostly)
 #' @examples \dontrun{
@@ -12,9 +12,9 @@
 #' }
 #' @export
 papers =
-function(discipline = NULL, upandcoming = FALSE, 
-  url = "http://api.mendeley.com/oapi/stats/papers/", 
-  key = getOption("MendeleyKey", 
+function(discipline = NULL, upandcoming = FALSE,
+  url = "http://api.mendeley.com/oapi/stats/papers/",
+  key = getOption("MendeleyKey",
   stop("need an API key for Mendeley")), ..., curl=getCurlHandle())
 {
   args = list(consumer_key = key)
@@ -22,7 +22,7 @@ function(discipline = NULL, upandcoming = FALSE,
      args$discipline = as.character(discipline)
   if(as.logical(upandcoming))
      args$upandcoming = 1L
-  
+
   tt = getForm(url, .params = args, .opts=list(...), curl=curl)
   ans = fromJSON(tt)
  structure(sapply(ans, `[[`, 2), names = sapply(ans, `[[`, 1))
