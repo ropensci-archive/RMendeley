fc543111b00c81fb91ee437f773172ac11026a97
Karthik Ram <karthik.ram@gmail.com>
2012-03-12 17:20:46 -0700

    Function to check for a valid Mendeley credential.

---
+++ R/is.mendeley.cred.r
@@ -0,0 +1,17 @@
+is.dropbox.cred <- function(mendeley_cred, response = TRUE) {
+    if (missing(mendeley_cred)) {
+        response <- FALSE
+    }
+    if (response) {
+
+       response <- ifelse(as.character(substitute(mendeley_cred)) %in% ls( envir = .GlobalEnv),TRUE,FALSE )
+   }
+    if (response) {
+        response <- ifelse(class(mendeley_cred) != "OAuth", FALSE, TRUE)
+    }
+    if (response) {
+        response <- ifelse(grep("dropbox", mendeley_cred$requestURL) != 1, FALSE,
+            TRUE)
+    }
+    return(response)
+}
