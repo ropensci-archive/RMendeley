a52a8301ad886ae31b0a45d677a555a94164818e
Karthik Ram <karthik.ram@gmail.com>
2012-03-12 20:31:44 -0700

    Function complete.

--- R/user_library.r
+++ R/user_library.r
@@ -1,1 +1,36 @@
 
+#'http://api.mendeley.com/oapi/library/
+#'
+#'<longer description>
+#'@param mendeley_cred Your Mendeley ROauth credentials.
+#'@param  page  Page number of search results
+#'@param  items  Number of items per page
+#'@keywords
+#'@seealso
+#'@return
+#'@alias
+#'@export
+#'@examples \dontrun{
+#' user_library(mendeley_cred)
+#' user_library(mendeley_cred, 2)
+#' user_library(mendeley_cred, 1, 25)
+#'}
+user_library <- function(mendeley_cred, page = NULL, items = NULL)
+{
+	lib_url <- ""
+	if(!is.mendeley.cred(mendeley_cred)) {
+		stop("Your Mendeley credentials are incorrect. Please run mendeley_auth() again")
+	}
+	lib_opts <- list()
+	if(!is.null(page)) {
+		lib_opts$page  <- page
+	}
+
+	if(!is.null(items)) {
+		lib_opts$items <-  items
+	}
+
+	library <- mendeley_cred$OAuthRequest("http://api.mendeley.com/oapi/library/", lib_opts, "GET")
+	library <- fromJSON(library)
+	return(library)
+}	 
