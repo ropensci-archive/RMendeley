f4f19520df26505315c301ec711dce8b5e4f9ee8
Karthik Ram <karthik.ram@gmail.com>
2012-03-12 18:40:06 -0700

    Function works fine.

--- R/user_groups.r
+++ R/user_groups.r
@@ -1,9 +1,19 @@
-
-
+#'Gives you a list of Mendeley groups
+#'@param mendeley_cred Your Mendeley OAUth credentials.
+#'@keywords
+#'@seealso
+#'@return
+#'@alias
+#'@export
+#'@examples \dontrun{
+#' mendeley_groups(mendeley_cred)
+#'}
 user_groups <- function(mendeley_cred)
 {
 	if(!is.mendeley.cred(mendeley_cred)) {
 		stop("Your Mendeley credentials are incorrect. Please run mendeley_auth() again")
 	}
-# function specific stuff
+	mendeley_groups <- mendeley_cred$OAuthRequest("http://api.mendeley.com/oapi/library/groups/", , "GET")
+	mendeley_groups <- fromJSON(mendeley_groups)
+   return(mendeley_groups)
 }
