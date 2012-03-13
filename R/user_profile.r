204f5bf9c40c73a2ee2c0ab7b85cab085cf2c13e
Karthik Ram <karthik.ram@gmail.com>
2012-03-12 18:04:02 -0700

    Mendeley user profile is now functional.

--- R/user_profile.r
+++ R/user_profile.r
@@ -1,8 +1,22 @@
-
-
-user_profile <- function(mendeley_cred)
+#'Returns profile of a Mendeley user
+#'
+#'@param mendeley_cred Mendeley OAUth credentials.
+#'@keywords
+#'@seealso
+#'@return
+#'@alias
+#'@export mendeley_autH
+#'@examples \dontrun{
+#' user_profile(mendeley_cred, '39369') # If left blank, it will retrieve your profile.
+#'}
+user_profile <- function(mendeley_cred, mendeley_user_id = NULL)
 {
 	if(!is.mendeley.cred(mendeley_cred)) {
 		stop("Your Mendeley credentials are incorrect. Please run mendeley_auth() again")
 	}
-# function specific stuff
+base_url <- "http://api.mendeley.com/oapi/profiles/info/"
+profile_url <- ifelse(is.null(mendeley_user_id),paste(base_url,"me",sep=""),paste(base_url,mendeley_user_id,sep=""))
+	user_profile_info <- mendeley_cred$OAuthRequest(profile_url, , "GET")
+	user_profile_info <- fromJSON(user_profile_info)
+	return(user_profile_info)
+}
