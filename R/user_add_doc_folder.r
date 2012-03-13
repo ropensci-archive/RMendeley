85a4c79195e20151ec57e539ad5fec0650567e31
Karthik Ram <karthik.ram@gmail.com>
2012-03-13 13:36:20 -0700

    Fleshed out function outline.

--- R/user_add_doc_folder.r
+++ R/user_add_doc_folder.r
@@ -1,1 +1,20 @@
-
+#'Allows authenticated users to add a document to an specific folder. The document must exists on the library 
+#'@param mendeley_cred <what param does>
+#'@param  folder_id <what param does>
+#'@param  doc_id <what param does>
+#'@keywords
+#'@seealso
+#'@return
+#'@alias
+#'@export
+#'@examples \dontrun{
+#'
+#'}
+user_add_doc_folder <- function(mendeley_cred, folder_id = NULL, doc_id = NULL)
+{
+	if(!is.mendeley.cred(mendeley_cred)) {
+		stop("Your Mendeley credentials are incorrect. Please run mendeley_auth() again")
+	}
+	#function specifics.
+}	
+# API: http://apidocs.mendeley.com/user-library-add-document-to-folder
