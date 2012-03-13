2ce5fd20cf06dd565355e631bfc9b8eddeac8fd4
Karthik Ram <karthik.ram@gmail.com>
2012-03-12 19:44:45 -0700

    Function is working

---
+++ R/user_tag_stats.r
@@ -0,0 +1,19 @@
+#'Allows authenticated users to get the most recent tags used in their library
+#'@param mendeley_cred <what param does>
+#'@keywords
+#'@seealso
+#'@return
+#'@alias
+#'@export
+#'@examples \dontrun{
+#' user_tag_stats(mendeley_cred)
+#'}
+user_tag_stats <- function(mendeley_cred)
+{
+	if(!is.mendeley.cred(mendeley_cred)) {
+		stop("Your Mendeley credentials are incorrect. Please run mendeley_auth() again")
+	}
+	tag_stats <- mendeley_cred$OAuthRequest("http://api.mendeley.com/oapi/library/tags/", , "GET")
+	tag_stats <- fromJSON(tag_stats)
+	return(tag_stats)
+}
