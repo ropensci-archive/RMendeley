f8bfd6ad9f8a23642c75b62fe85ce7d1d6d42a9d
Karthik Ram <karthik.ram@gmail.com>
2012-03-12 19:50:21 -0700

    Changed return to data.frame to improve readability.

--- R/user_author_stats.r
+++ R/user_author_stats.r
@@ -10,12 +10,12 @@
 #'@examples \dontrun{
 #' author_stats(mendeley_oauth_cred)
 #'}
-author_stats <- function(mendeley_cred)
+user_author_stats <- function(mendeley_cred)
 {
 	if(!is.mendeley.cred(mendeley_cred)) {
 		stop("Your Mendeley credentials are incorrect. Please run mendeley_auth() again")
 	}
 	au_stats <- mendeley_cred$OAuthRequest("http://api.mendeley.com/oapi/library/authors/", , "GET")
 	au_stats <- fromJSON(au_stats)
-	return(au_stats)
+	return(ldply(au_stats))
 }
