776f958628365b0fd3730aec9e18e3d5cf1c7a3f
Karthik Ram <karthik.ram@gmail.com>
2012-03-12 17:29:53 -0700

    Added roxygen documentation.

--- R/mendeley_auth.r
+++ R/mendeley_auth.r
@@ -1,5 +1,18 @@
-
-
+#'Function to obtain OAuth tokens from Mendeley to faciliate user specific methods.
+#'
+#'<longer description>
+#'@param cKey Consumer key. can be supplied here or read from Options()
+#'@param  cSecret Consumer secret. can be supplied here or read from Options()
+#'@keywords
+#'@seealso
+#'@return OAUth credential.
+#'@alias
+#'@export
+#'@examples \dontrun{
+#' mendeley_auth('YOUR_CONSUMER_KEY', 'YOUR_CONSUMER_SECRET')
+#' mendeley_auth() # If your keys are stored in options using
+#' as MendeleyKey and MendeleyPrivateKey.
+#'}
 mendeley_auth <- function(cKey = NULL, cSecret=NULL) {
     if (is.null(cKey) && is.null(cSecret)) {
         cKey = getOption("MendeleyKey")
@@ -15,4 +28,4 @@ mendeley_cred$handshake(post = FALSE, verbose = FALSE)
 
 cat("We recommend that you save mendeley_cred to disk using the save command. Doing so will and reloading your credentials from disk will allow you to skip the auth step every time")
 }
-# Notes: requires new ROAuth which is not yet on CRAN.
+# Notes: requires new ROAuth (ROAuth_0.91.0) which is not yet on CRAN. 
