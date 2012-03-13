440bc1afcad4dd51facc4acd057a362eb65505ae
Karthik Ram <karthik.ram@gmail.com>
2012-03-12 20:44:21 -0700

    Added function outline. Not yet functional.

--- R/user_delete_folder.r
+++ R/user_delete_folder.r
@@ -1,9 +1,19 @@
-
-
-user_delete_folder <- function(mendeley_cred)
-{
+#'Allows authenticated users to remove a folder from their library. Documents contained in the id.
+#'
+#'<longer description>
+#'@param mendeley_cred <what param does>
+#'@param  id Id of the folder to be removed.
+#'@keywords
+#'@seealso
+#'@return
+#'@alias
+#'@export
+#'@examples \dontrun{
+#'
+#'}
+user_delete_folder <- function(mendeley_cred, id = NULL) {
 	if(!is.mendeley.cred(mendeley_cred)) {
 		stop("Your Mendeley credentials are incorrect. Please run mendeley_auth() again")
 	}
-# function specific stuff
+# http://api.mendeley.com/oapi/library/folders/<id>/
 }
