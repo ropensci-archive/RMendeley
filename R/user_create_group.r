8609b1d89fb759c2ef02ddd550e047c0081a0d06
Karthik Ram <karthik.ram@gmail.com>
2012-03-12 20:43:06 -0700

    Fleshed out function some more but not yet working.

--- R/user_create_group.r
+++ R/user_create_group.r
@@ -1,8 +1,22 @@
 
-user_create_group <- function(mendeley_cred)
-{
+#'This function will create a new group in the user's library. 
+#'@param mendeley_cred Your Mendeley OAuth credentials.
+#'@param  group Name of your group.
+#'@param  type Three types of groups are allowed: 'private', 'invite' and 'open'.
+#'@keywords
+#'@seealso
+#'@return
+#'@alias
+#'@export
+#'@examples \dontrun{
+#'
+#'}
+user_create_group <- function(mendeley_cred, group = NULL, type = NULL) {
 	if(!is.mendeley.cred(mendeley_cred)) {
 		stop("Your Mendeley credentials are incorrect. Please run mendeley_auth() again")
 	}
-# function specific stuff
+if(is.null(group)) {
+	stop("You did not specify a group name", call.= FALSE)
+}
+# http://api.mendeley.com/oapi/library/groups/
 }
