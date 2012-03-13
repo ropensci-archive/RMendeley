2032bf537c0b2578e669bb28c71bbadfd1973279
Karthik Ram <karthik.ram@gmail.com>
2012-03-12 20:48:47 -0700

    Function outline. Not yet functional.

--- R/user_upload_file.r
+++ R/user_upload_file.r
@@ -1,7 +1,17 @@
-
-user_upload_file  <- function(mendeley_cred)
-{
+#'Allows authenticated users to attach files to a document in their library. 
+#'@param mendeley_cred Your Mendeley OAuth credentials.
+#'@param  id Id of the document we want to attach the file to.
+#'@keywords
+#'@seealso
+#'@return
+#'@alias
+#'@export
+#'@examples \dontrun{
+#'
+#'}
+user_upload_file  <- function(mendeley_cred, id = NULL) {
 	if(!is.mendeley.cred(mendeley_cred)) {
 		stop("Your Mendeley credentials are incorrect. Please run mendeley_auth() again")
 	}
+	# function stuff here.
 }	
