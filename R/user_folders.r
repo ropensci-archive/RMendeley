cb6e7e5c0a84c34e2377cb0e24d39e4fdeacbf72
Karthik Ram <karthik.ram@gmail.com>
2012-03-12 18:50:47 -0700

    Function is complete.

--- R/user_folders.r
+++ R/user_folders.r
@@ -1,9 +1,19 @@
-
-
+#' Retuns a list of your Mendeley folders with sizes
+#'@param mendeley_cred Your Mendeley ROAuth credential.
+#'@keywords
+#'@seealso
+#'@return list
+#'@alias
+#'@export
+#'@examples \dontrun{
+#' user_folders(mendeley_cred)
+#'}
 user_folders <- function(mendeley_cred)
 {
 	if(!is.mendeley.cred(mendeley_cred)) {
 		stop("Your Mendeley credentials are incorrect. Please run mendeley_auth() again")
 	}
-# function specific stuff
+	mendeley_folders <- mendeley_cred$OAuthRequest("http://api.mendeley.com/oapi/library/folders/", , "GET")
+	mendeley_folders <- fromJSON(mendeley_folders)
+	return(mendeley_folders)
 }
