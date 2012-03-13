b1295bea0a2802571c07756da51fdf5e73049b83
Karthik Ram <karthik.ram@gmail.com>
2012-03-12 19:51:43 -0700

    Finished user_contacts()

--- R/user_contacts.r
+++ R/user_contacts.r
@@ -1,9 +1,19 @@
-
-
+#'Function that returns a list of your Mendeley contacts
+#'@param mendeley_cred Your Mendeley OAuth credentials.
+#'@keywords
+#'@seealso
+#'@return list
+#'@alias
+#'@export
+#'@examples \dontrun{
+#' user_contacts(mendeley_cred)
+#'}
 user_contacts <- function(mendeley_cred)
 {
 	if(!is.mendeley.cred(mendeley_cred)) {
 		stop("Your Mendeley credentials are incorrect. Please run mendeley_auth() again")
 	}
-# function specific stuff
+	mendeley_contacts <- mendeley_cred$OAuthRequest("http://api.mendeley.com/oapi/profiles/contacts/", , "GET")
+	mendeley_contacts <- fromJSON(mendeley_contacts)
+   return(mendeley_contacts)
 }
