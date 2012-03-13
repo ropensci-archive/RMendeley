fcd5f6906ab8f76fd5786ecf11f7a1096521794b
Karthik Ram <karthik.ram@gmail.com>
2012-03-12 20:47:04 -0700

    Fleshed out function. Not yet working.

--- R/user_delete_group.r
+++ R/user_delete_group.r
@@ -1,7 +1,17 @@
-
-
-user_delete_group <- function(mendeley_cred)
-{
+#'The following methods will essentially delete a group from the user's library. 
+#'
+#'<longer description>
+#'@param mendeley_cred <what param does>
+#'@param  group_id = NULL <what param does>
+#'@keywords
+#'@seealso
+#'@return
+#'@alias
+#'@export
+#'@examples \dontrun{
+#'
+#'}
+user_delete_group <- function(mendeley_cred, group_id = NULL) {
 	if(!is.mendeley.cred(mendeley_cred)) {
 		stop("Your Mendeley credentials are incorrect. Please run mendeley_auth() again")
 	}
