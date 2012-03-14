35b0f2064ce745d44e8235d9d6d7dda0b9bd029c
Karthik Ram <karthik.ram@gmail.com>
2012-03-13 23:02:47 -0700

    function skeletion done. still doesn't work.

--- R/user_delete_group.r
+++ R/user_delete_group.r
@@ -15,6 +15,11 @@ user_delete_group <- function(mendeley_cred, group_id = NULL) {
 	if(!is.mendeley.cred(mendeley_cred)) {
 		stop("Your Mendeley credentials are missing or incorrect. Please run mendeley_auth() again")
 	}
-# function specific stuff
+if(is.null(group_id)) {
+	stop("Group id is missing", call. = FALSE)
+ }
+del_group_url <- paste("http://api.mendeley.com/oapi/library/groups/", id, "/", sep="")
+delete_group <- mendeley_cred$OAuthRequest(del_group_url,  , "DELETE")
+return (delete_group)
 }
 # API: http://apidocs.mendeley.com/home/user-specific-methods/user-library-delete-group
