6faf9ca1869cc3d20079f02288971723259dd9f8
Karthik Ram <karthik.ram@gmail.com>
2012-03-21 12:25:34 -0700

    I'm removing user specific methods from the main branch so it doesn't break install_github. All new functions remain in the dev branch.

--- R/user_download_file.r
+++
@@ -1,8 +0,0 @@
-
-user_download_file  <- function(mendeley_cred)
-{
-	if(!is.mendeley.cred(mendeley_cred)) {
-		stop("Your Mendeley credentials are missing or incorrect. Please run mendeley_auth() again")
-	}
-}	
-# API: http://apidocs.mendeley.com/home/user-specific-methods/download-file
