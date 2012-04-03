c24fd6fa4e3a83a4419743456d22687385deee00
Karthik Ram <karthik.ram@gmail.com>
2012-04-03 15:19:05 -0700

    function is complete and works.

--- R/user_groups.r
+++ R/user_groups.r
@@ -1,19 +1,18 @@
 #'Gives you a list of Mendeley groups
-#'@param mendeley_cred Your Mendeley OAUth credentials.
-#'@keywords
-#'@seealso
+#'@param mendeley_cred OAuth object of class MendeleyCredentials
 #'@return data.frame
-#'@alias
+#' @param curl If using in a loop, call getCurlHandle() first and pass
+#'  the returned value in here (avoids unnecessary footprint)
+#' @param ... optional additional curl options (debugging tools mostly).
 #'@export
 #'@examples \dontrun{
 #' mendeley_groups(mendeley_cred)
 #'}
-user_groups <- function(mendeley_cred)
+user_groups <- function(mendeley_cred, ..., curl = getCurlHandle())
 {
-	if(!is.mendeley.cred(mendeley_cred)) {
-		stop("Your Mendeley credentials are missing or incorrect. Please run mendeley_auth() again")
-	}
-	mendeley_groups <- mendeley_cred$OAuthRequest("http://api.mendeley.com/oapi/library/groups/", , "GET")
+    if (!is(mendeley_cred, "MendeleyCredentials") ||  missing(mendeley_cred))
+        stop("Invalid or missing Mendeley credentials. ?mendeley_auth for more information.", call.= FALSE)
+	mendeley_groups <- OAuthRequest(mendeley_cred, "http://api.mendeley.com/oapi/library/groups/", , "GET")
 	mendeley_groups <- fromJSON(mendeley_groups)
 	mendeley_groups <- ldply(mendeley_groups, function(x) as.data.frame(x))
     return(mendeley_groups)
