ee41f5aae3490541e2effacda6acf1415effba38
Karthik Ram <karthik.ram@gmail.com>
2012-04-03 14:55:13 -0700

    function is working.

--- R/user_delete_folder_doc.r
+++ R/user_delete_folder_doc.r
@@ -10,14 +10,19 @@
 #'@examples \dontrun{
 #'
 #'}
-user_delete_folder_doc <- function(mendeley_cred, folder_id = NULL, doc_id = NULL) {
-	if(!is.mendeley.cred(mendeley_cred)) {
-		stop("Your Mendeley credentials are missing or incorrect. Please run mendeley_auth() again")
-	}
-if(is.null(folder_id)) {
-	stop("Group id is missing", call. = FALSE)
- }
-del_folder_doc <- paste("http://api.mendeley.com/oapi/library/folders/", folder_id, "/", doc_id, "/", sep="")
-delete_folder_doc <- mendeley_cred$OAuthRequest(del_folder_doc,  , "DELETE")
-return (delete_folder_doc)
+user_delete_folder_doc <- function(mendeley_cred, folder_id = NULL, doc_id = NULL,..., curl=getCurlHandle()) {
+    if (!is(mendeley_cred, "MendeleyCredentials") ||  missing(mendeley_cred))
+        stop("Invalid or missing Mendeley credentials. ?mendeley_auth for more information.", call.= FALSE)
+
+  if(!is.null(folder)) {
+     u = sprintf("http://api.mendeley.com/oapi/library/folders/%s/%s/", folder_id, doc_id)
+  } else
+     u =  as(doc, "URL")
+ deleted_doc <- OAuthRequest(mendeley_cred, u, method = "DELETE", ..., curl = curl, followlocation = TRUE)
+if(deleted_doc=="") {
+	cat("successfully deleted document", doc_id, "from folder ", folder_id, "\n")
+			} else {
+				return(fromJSON(deleted_doc))
+			}
 }
+
