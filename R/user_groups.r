f4c72089f6fc8e7a45bc2d7ed4bf590de9eeefa2
Karthik Ram <karthik.ram@gmail.com>
2012-04-14 10:49:48 -0700

    Moved in GetGroupID() function here.

--- R/user_groups.r
+++ R/user_groups.r
@@ -20,3 +20,38 @@ myGroups <- function(mendeley_cred = NULL, ..., curl = getCurlHandle()) {
     mendeley_groups <- ldply(mendeley_groups, function(x) as.data.frame(x))
     return(mendeley_groups)
 }
+
+
+#' Get a Group ID from group name
+#'
+#' Meant for internal use.
+#' @param mendeley_cred <what param does>
+#' @param  group Name of group
+#' @param ... <what param does>
+#' @param  curl = getCurlHandle() <what param does>
+#' @export
+#' @return numeric
+#' @examples \dontrun{
+#' getGroupID(mendeley_cred, '')
+#'}
+getGroupID <- function(mendeley_cred = NULL, group = NULL,..., curl = getCurlHandle()) {
+
+ if (!is(mendeley_cred, "MendeleyCredentials") || missing(mendeley_cred))
+        stop("Invalid or missing Mendeley credentials. ?mendeley_auth for more information.",
+            call. = FALSE)
+
+if (is.null(group)) {
+        stop("Group name is missing", call. = FALSE)
+    }
+
+groups <- myGroups(mendeley_cred, curl = curl)
+selected_group <- groups[which(groups$name==group),]
+
+if(dim(selected_group)[1]==0)
+    stop("No matching group found", call.=F)
+
+if(dim(selected_group)[1]>2)
+    stop("Multiple groups found. Please check the name", call.=F)
+
+return(selected_group$id)
+}
